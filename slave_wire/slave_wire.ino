f8d8bcaa2aa006b58d07e3f34fd8e0a961dc6575
Ingo Veith <ingoveith@gmx.de>
2020-01-17 13:49:22 +0100

    test wire

---
+++ slave_wire/slave_wire.ino
@@ -0,0 +1,86 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+
+// i2c address for master-slave communication
+#define SLAVE_ADDRESS 0x04
+#define CONFIG_SIZE 10
+
+#define SD_CS_PIN 4
+#define CONFIG_FILENAME "config.txt"
+#define FAIL_LED 5
+#define OK_LED 6
+
+File myFile;
+
+
+void setup() {
+
+  pinMode (FAIL_LED, OUTPUT);
+  pinMode (OK_LED, OUTPUT);
+  digitalWrite(FAIL_LED, HIGH);
+  digitalWrite(OK_LED, HIGH);
+
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+
+  if (SD.begin(SD_CS_PIN)) {
+    digitalWrite(FAIL_LED, LOW);
+  }
+  else {
+    digitalWrite(OK_LED, LOW);
+  }
+
+
+  Serial.println("Ready!");
+}
+
+void receiveData(int byteCount) {
+  String data;
+  while (Wire.available()) {
+    char foo = Wire.read();
+    data += foo;
+  }
+  // save Data on SD-Card
+  sdPersist(data);
+
+}
+
+void sendData() {
+  String configString = sdLoad();
+  char data[CONFIG_SIZE];
+  configString.toCharArray(data, CONFIG_SIZE + 1);
+  Wire.write(data);
+}
+
+void loop() {
+  delay(100);
+}
+
+void sdPersist(String data) {
+  SD.remove(CONFIG_FILENAME);
+  myFile = SD.open(CONFIG_FILENAME, FILE_WRITE);
+  if (myFile) {
+    myFile.print(data);
+    // close the file:
+    myFile.close();
+  }
+}
+
+String sdLoad() {
+  String data;
+  myFile = SD.open(CONFIG_FILENAME);
+  if (myFile) {
+    while (myFile.available()) {
+      char character = myFile.read();
+      data += character;
+    }
+    // close the file:
+    myFile.close();
+  }
+  return data;
+}
